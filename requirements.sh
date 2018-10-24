e5e4355dae5d420dd8dc7822a6c71ee09b63be06
Yashas Roy <yashas@datacamp.com>
2018-10-24 17:12:35 -0400

    install vega_datasets

--- requirements.sh
+++ requirements.sh
@@ -4,6 +4,7 @@ pip3 install dill==0.2.8.2 --target $SHARED_PYTHON_PATH --upgrade
 pip3 install ipython==5.0.0 --target $SHARED_PYTHON_PATH --upgrade
 pip3 install numpy==1.15.3 --target $SHARED_PYTHON_PATH --upgrade
 pip3 install pandas==0.23.2 --target $SHARED_PYTHON_PATH --upgrade
+pip3 install vega_datasets
 pip3 install markdown2==2.3.6 --target $SHARED_PYTHON_PATH --upgrade
 pip3 install Jinja2==2.10 --target $SHARED_PYTHON_PATH --upgrade
 pip3 install git+https://github.com/datacamp/pythonwhat@$PYTHONWHAT_VERSION --upgrade --no-dependencies
